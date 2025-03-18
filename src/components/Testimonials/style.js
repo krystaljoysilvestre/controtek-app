0bbdda4ebb6110a723f3b160bee4ec4b2e5b53ff
Krystal Joy Silvestre <kjmsilvestre@gmail.com>
2025-03-18 16:01:38 +0800

    feat: testimonial mobile view

--- src/components/Testimonials/style.js
+++ src/components/Testimonials/style.js
@@ -1,13 +1,12 @@
 import styled from "styled-components";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { device } from "../../constants/devices";
 
 export const SwiperContainer = styled.div`
   position: relative;
 `;
 
 export const TestimonialsIcon = styled.div`
-  width: 180px;
-  height: 180px;
   background: #ffffff;
   display: flex;
   align-items: center;
@@ -16,6 +15,44 @@ export const TestimonialsIcon = styled.div`
   z-index: 3;
   top: 0;
   left: 0;
+  width: 64px;
+  height: 64px;
+
+  svg {
+    width: 16px;
+  }
+
+  @media ${device.mobileL} {
+    width: 80px;
+    height: 80px;
+
+    svg {
+      width: 24px;
+    }
+  }
+
+  @media ${device.tablet} {
+    width: 100px;
+    height: 100px;
+
+    svg {
+      width: 30px;
+    }
+  }
+
+  @media (min-width: 991px) {
+    width: 130px;
+    height: 130px;
+
+    svg {
+      width: 45px;
+    }
+  }
+
+  @media ${device.laptop} {
+    width: 180px;
+    height: 180px;
+  }
 `;
 
 export const StyledSwiper = styled(Swiper)`
@@ -40,9 +77,20 @@ export const SwiperControlsWrapper = styled.div`
 
 export const SwiperControls = styled.div`
   width: 100%;
-  max-width: 720px;
-
+  max-width: 250px;
   display: flex;
+
+  @media ${device.mobileL} {
+    max-width: 320px;
+  }
+
+  @media ${device.tablet} {
+    max-width: 520px;
+  }
+
+  @media ${device.laptop} {
+    max-width: 720px;
+  }
 `;
 
 export const LeftControl = styled.div`
@@ -82,7 +130,15 @@ export const Center = styled.div`
 `;
 
 export const Container = styled.div`
-  padding: 0 56px;
+  padding: 0 16px;
+
+  @media ${device.tablet} {
+    padding: 0 40px;
+  }
+
+  @media ${device.laptop} {
+    padding: 0 56px;
+  }
 `;
 
 export const Row = styled.div`
@@ -93,13 +149,25 @@ export const Row = styled.div`
 
 export const Content = styled.div`
   width: 100%;
-  max-width: 720px;
+  max-width: 250px;
   display: flex;
   flex-direction: column;
   gap: 24px;
-  margin: 0 56px;
   height: 100%;
   justify-content: center;
+
+  @media ${device.mobileL} {
+    max-width: 320px;
+  }
+
+  @media ${device.tablet} {
+    max-width: 520px;
+  }
+
+  @media ${device.laptop} {
+    margin: 0 56px;
+    max-width: 720px;
+  }
 `;
 
 export const Testimonial = styled.div`
@@ -107,21 +175,32 @@ export const Testimonial = styled.div`
   leading-trim: both;
   text-edge: cap;
   font-family: "Funnel Sans";
-  font-size: 24px;
+  font-size: 12px;
   font-style: normal;
   font-weight: 300;
   line-height: 130%;
-  letter-spacing: 0.24px;
+  letter-spacing: 0.12px;
+
+  @media ${device.tablet} {
+    font-size: 24px;
+    letter-spacing: 0.24px;
+  }
 `;
 
 export const Author = styled.h4`
   margin: 0;
   color: #000000;
   font-family: "Funnel Display";
-  font-size: 16px;
+  font-size: 12px;
   font-style: normal;
-  font-weight: 700;
+  font-weight: 800;
   line-height: normal;
-  letter-spacing: 0.8px;
+  letter-spacing: 0.6px;
   text-transform: uppercase;
+
+  @media ${device.tablet} {
+    font-size: 16px;
+    font-weight: 700;
+    letter-spacing: 0.8px;
+  }
 `;
